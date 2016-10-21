0d81ef2f5b03d46714d2de55055cc87b3d3ca5a6
borjamc <borjacofrentino@gmail.com>
2016-10-21 08:46:22 +0200

    Update ia.php

--- pInvadersF2-master/lib/ia.php
+++ pInvadersF2-master/lib/ia.php
@@ -22,24 +22,30 @@ public function randomPos(){
 }
 
 
-//Setter
+//Setter con Control
 
 public function setColumna($col)
 {
-	if ($col>3){
-	$this->columnas=3;
-	}else{
+	if ($col<=3 && $col>=0){
 	$this->columnas=$col;
+	}else if ($col > 3){
+	$this->columnas=3;
+	}else if ($col < 0){
+	$this->columnas=0;
 	}
 }
 
 public function setFila($fil)
 {
-	if ($fil<0){
+	if ($fil <=3 && $fil>=0){
+	$this->fila=$fil;
+	}else if ($fil > 3){
+	$this->fila=3;
+	}else if ($fil < 0){
 	$this->fila=0;
-	}else{
-	$this->fila=$fil;	
 	}
+	
+}
 	
 }
 
